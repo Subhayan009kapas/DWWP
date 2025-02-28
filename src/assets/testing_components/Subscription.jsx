8d90b03a9e8c59c8b9416ba578b6333be8a77415
Subhayan009kapas <subhayankapas47@gmail.com>
2025-03-01 00:56:37 +0530

    2nd commit

--- src/assets/testing_components/Subscription.jsx
+++ src/assets/testing_components/Subscription.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import TopUpCard from "./topUpCard";
+import TopUpCard from "./TopUpCard";
 import "./Subscription.css";
 
 const Subscription = () => {
